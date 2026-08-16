46360cc3422c4d1180af3e93f6b12e90229ea79c
Tun Lin <lintun2003@gmail.com>
2026-08-16 11:01:36 -0400

    fix deploy

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ import home from "./routes/home.js";
 import meat from "./routes/meat.js";
 import add from "./routes/add-meat.js";
 import type from "./routes/type.js";
-//test deploy
 const app = express();
 
 app.set("view engine", "ejs");
