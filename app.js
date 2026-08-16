509324d5c49fdc7709f9463aadc8ebc18984a631
Tun Lin <lintun2003@gmail.com>
2026-08-16 11:05:47 -0400

    add private key

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import meat from "./routes/meat.js";
 import add from "./routes/add-meat.js";
 import type from "./routes/type.js";
 const app = express();
-
+//
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
